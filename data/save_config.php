42ca0f2dfb2ef63d6c8dfff241b0efa973ca89cb
Alessandro <gubi.ale@gotanotherway.com>
2012-09-08 08:23:57 +0200

    Correct alert on saved file

--- data/save_config.php
+++ data/save_config.php
@@ -5,7 +5,7 @@ if(isset($_POST["script"]) && trim($_POST["script"]) !== ""){
 	$save_dir = "../../etc/config/eigennet";
 	$fh = fopen($save_dir, "w");
 	if(fwrite($fh, stripslashes($_POST["script"]))){
-		print "Salvato!\n\nVedi: http://www.gotanotherway.com/apps/ninux/etc/config/eigennet";
+		print "saved";
 	} else {
 		print "error";
 	}

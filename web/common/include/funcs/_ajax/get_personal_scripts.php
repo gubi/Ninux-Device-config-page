93e499037d44bc0abe87abaff775da6a71a04851
Alessandro <gubi.ale@iannc.org>
2012-09-26 03:51:13 +0300

    Update web/common/include/funcs/_ajax/get_personal_scripts.php

--- web/common/include/funcs/_ajax/get_personal_scripts.php
+++ web/common/include/funcs/_ajax/get_personal_scripts.php
@@ -14,7 +14,7 @@ if(isset($_POST)){
 				$user_id = $dato_user["id"];
 				
 				// Check existing session
-				$check_session = $pdo->prepare("select * from `ninux_cp_personal_configs` where `user_id` = '" . addslashes($user_id) . "' and `session_id` = '" . addslashes($session_id) . "' order by `date` desc");
+				$check_session = $pdo->prepare("select * from `ninux_cp_personal_configs` where `user_id` = '" . addslashes($user_id) . "' order by `date` desc");
 				if($check_session->execute()){
 					if($check_session->rowCount() > 0){
 						$id = -1;
@@ -22,8 +22,8 @@ if(isset($_POST)){
 							$id++;
 							$config_items["script"][$id]["id"] = $dato_session["id"];
 							$config_items["script"][$id]["session_id"] = $dato_session["session_id"];
-							$config_items["script"][$id]["title"] = stripslashes($dato_session["title"]);
-							$config_items["script"][$id]["config"] = stripslashes($dato_session["config"]);
+							$config_items["script"][$id]["title"] = stripslashes(stripslashes($dato_session["title"]));
+							$config_items["script"][$id]["config"] = stripslashes(stripslashes($dato_session["config"]));
 							$config_items["script"][$id]["is_temp"] = $dato_session["is_temp"];
 							$config_items["script"][$id]["date"] = $dato_session["date"];
 						}
